d16de97fac1b9fd5ff7a34041694431bf44f9446
Shreya Sharma <sharma-shreya@outlook.com>
2026-06-13 19:20:35 +0530

    Updating files

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,94 @@
+import { createContext, useContext, useEffect, useState } from "react"
+import axios from "axios";
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { useLocation, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+// Axios is used to set defaullt url, backend url
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
+
+export const AppContext = createContext() //app context
+
+// this AppProvider allows to access all the global states and functions by using context
+export const AppProvider = ({ children }) => {
+
+    const [isAdmin, setIsAdmin] = useState(false)
+    const [shows, setShows] = useState([])
+    const [favoriteMovies, setFavoriteMovies] = useState([])
+
+    const image_base_url = import.meta.env.VITE_TMDB_IMAGE_BASE_URL;
+
+    const {user} = useUser()
+    const {getToken} = useAuth()
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    //fetch admin function- will be executed whenever we open the webpage or whenever the user is changed
+    const fetchIsAdmin = async () => {
+        try{
+            const {data} = await axios.get('/api/admin/is-admin', {headers: {Authorization: `Bearer ${await getToken()}`}})
+
+            setIsAdmin(data.isAdmin)
+
+            if(!data.isAdmin && location.pathname.startsWith('/admin')){
+                navigate('/')
+                toast.error('You are not authorized to access admin dashboard')
+            }
+        } catch(error) {
+            console.error(error)
+        }
+    }
+
+    const fetchShows = async () => {
+        try{
+            const { data } = await axios.get('/api/show/all')
+            if(data.success){
+                setShows(data.shows)
+            }else{
+                toast.error(data.message)
+            }
+        } catch(error) {
+            console.error(error)
+        }
+    }
+
+    const fetchFavoriteMovies = async () => {
+        try{
+            const { data } = await axios.get('/api/user/favorites', {headers: {Authorization:  `Bearer ${await getToken()}`}})
+
+            if(data.success){
+                setFavoriteMovies(data.movies)
+            }else{
+                toast.error(data.message)
+            }
+        }catch(error){
+            console.error(error)
+        }
+    }
+
+    useEffect(()=>{
+        fetchShows()
+    }, [])
+
+    useEffect(()=>{
+        if(user){
+            fetchIsAdmin()
+            fetchFavoriteMovies()
+        }
+    }, [user])
+    
+    // passing functions and state variables through this value object so that we can access it in any other component
+    const value = {axios,
+        fetchIsAdmin,
+        user, getToken, navigate, isAdmin, shows,
+        favoriteMovies, fetchFavoriteMovies, image_base_url,
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext = () => useContext(AppContext)
